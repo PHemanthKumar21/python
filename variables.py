f8953bb9ece97ebce553713265d6c692431ccb3f
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-07 16:19:18 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,121 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=3
+b=7
+print(a+b)
+10
+c=30
+print(c)
+30
+Z=80
+print(z)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(z)
+NameError: name 'z' is not defined. Did you mean: 'Z'?
+Z=80
+print(Z)
+80
+c=1,2,4
+print(c)
+(1, 2, 4)
+a,b,c=1,2,3,
+print(a,b,c)
+1 2 3
+name="hemanth"
+print(name)
+hemanth
+city="vja"
+print(city)
+vja
+country="india"
+print(country)
+india
+2=30
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a2=30
+print(a2)
+30
+4a=66
+SyntaxError: invalid decimal literal
+a123456=100
+print(a123456)
+100
+@=50
+SyntaxError: invalid syntax
+$=45
+SyntaxError: invalid syntax
+_=18
+print(_)
+18
+_c=6
+print(_c)
+6
+ f=45
+ 
+SyntaxError: unexpected indent
+if=29
+SyntaxError: invalid syntax
+first name="hemanth"
+SyntaxError: invalid syntax
+first_name="hemanth"
+print(first_name)
+hemanth
+firstname="hemanth"
+print(firstname)
+hemanth
+a=3;b=5
+print(a+b)
+8
+a,b=5,8
+print(a+b)
+13
+a=2,4,6,7,8,9
+print(a)
+(2, 4, 6, 7, 8, 9)
+a,b,c=2,5,6
+print(a,b,c)
+2 5 6
+a,b,c=3,2,7,9,0,9
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    a,b,c=3,2,7,9,0,9
+ValueError: too many values to unpack (expected 3, got 6)
+>>> a,b,c=(4,3,8)
+>>> print(a,b,c)
+4 3 8
+>>> a=4
+>>> print(a)
+4
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+>>> fname="hemanth"
+>>> lname="naidu"
+>>> print(fname+lname)
+hemanthnaidu
+>>> print(fname+" "+lname)
+hemanth naidu
+>>> print(fname,lname)
+hemanth naidu
+>>> hemanth naidu
+SyntaxError: invalid syntax
+>>> name="hemanth"
+>>> print(name)
+hemanth
+>>> NMAE="hemanth"
+>>> print(NAME)
+Traceback (most recent call last):
+  File "<pyshell#63>", line 1, in <module>
+    print(NAME)
+NameError: name 'NAME' is not defined
+>>> NAME="hemanth"
+>>> print(NAME)
+hemanth
+>>> Name="hemanth"
+>>> print(Name)
+hemanth
