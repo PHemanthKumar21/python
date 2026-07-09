72a43e8f3b6936f4aecaece672b23388727c4ef8
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-09 14:02:59 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #datatypes
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=7.3
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> c=="hemanth"
+False
+>>> c="hemanth"
+>>> type(c)
+<class 'str'>
+>>> x=5+6j
+>>> type(x)
+<class 'complex'>
+>>> x=4j+9
+>>> typr(x)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    typr(x)
+NameError: name 'typr' is not defined. Did you mean: 'type'?
+>>> x=4j+9
+>>> type(x)
+<class 'complex'>
+>>> c=True
+>>> type(c)
+<class 'bool'>
+>>> d=False
+>>> type(d)
+<class 'bool'>
