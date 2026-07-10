ee6ce40129a2b542d6bd449439d9d8ee3277fff8
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-10 15:59:38 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,53 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a="vijayawada"
+a[3]
+'a'
+a[5]
+'a'
+a[6]
+'w'
+a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'vijaya'
+a="hemanth kumar"
+a[6]
+'h'
+a[9]
+'u'
+>>> a[3]+a[]4]+a[5]
+SyntaxError: unmatched ']'
+>>> a[1]+a[2]
+'em'
+>>> a[4]+a[5]
+'nt'
+>>> a='iam learning python"
+SyntaxError: unterminated string literal (detected at line 1)
+>>> a="i am learing python"
+>>> a="i am learing python"
+>>> a="i am learning python"
+>>> a[5]+a[6]+a[7]+a[8]+a[9]
+'learn'
+>>> a[14]+a[15]+a[16]+a[17]+a[18]+a[19]
+'python'
+>>> 
+>>> a="i love codegnan"
+>>> a[1]+a[2]+a[3]+a[4]
+' lov'
+>>> a[2]+a[3]+a[4]+a[5]
+'love'
+>>> a[7]+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]
+'codegnan'
+>>> a="vijayawada is a royal city"
+>>> a[-10]+a[-9]+a[-8]+a[-7]+a[-6]
+'royal'
+>>> a[-4]+a[-3]+a[-2]+a[-1]
+'city'
+>>> a[-15]+a[-14]
+'is'
+>>> a="vizag is a city of destiny"
+>>> a[-15]+a[-14]+a[-13]+a[-12]
+'city'
+>>> a[-7]+a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'destiny'
+>>> a[-26]+a[-25]+a[-24]+a[-23]+a[-22]
+'vizag'
