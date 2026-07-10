1dd0804d7b1a7070d56c09ccb89f364df8205701
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-10 16:00:21 +0530

    Add files via upload

---
+++ striding.py
@@ -0,0 +1,26 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #striding
+>>>  a="data science"
+...  
+SyntaxError: unexpected indent
+>>> a="data science"
+>>> a[::]
+'data science'
+>>> a[::1]
+'data science'
+>>> a[::2]
+'dt cec'
+>>> a[3:6:1]
+'a s'
+>>> b="hemanth kumar"
+>>> 
+>>> b[2:12:4]
+'mhm'
+>>> b[3:8:1]
+'anth '
+>>> b[-2:-5:-2]
+'au'
+>>> b[-2:-8:6]
+''
+
