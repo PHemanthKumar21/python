548bb1ea5cf2d794703bcd4caa104916adac1230
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-17 15:49:55 +0530

    Add files via upload

---
+++ if-else tasks.py
@@ -0,0 +1,72 @@
+#voting
+
+'''age=int(input("enter a age"))
+if age>18:
+      print("eligible for vote")
+else:
+    print("not eligible")'''
+
+
+
+#even or odd
+
+'''n=int(input("enter a value"))
+if n % 2==0:
+      print("it is even")
+else:
+    print("it is odd")'''
+
+
+
+#leap year
+
+'''year=int(input("enter a value"))
+if year % 4==0:
+    print("leap year")
+else:
+    print("not leap year")'''
+
+
+
+#guest code
+
+'''name=str(input("enter a name"))
+if name=="hemanth":
+    print("welcome hemanth")
+else:
+    print("welcome guest")'''
+
+
+
+#5names
+
+'''names=["syam","kiran","roop","sai","hemanth"]
+a=(input("enter the name"))
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+
+
+#vowels
+'''letters=["a","e","i","o","u"]
+w=input("enter a letter")
+if w in letters:
+    print("it is vowel")
+else:
+    print("it is not vowel")'''
+        
+
+
+
+
+
+
+    
+
+
+
+
+
+        
